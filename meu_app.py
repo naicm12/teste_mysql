f388b039c7767f6ed70f2a5a411a2d0306b86ef3
naicm12 <163166528+naicm12@users.noreply.github.com>
2024-03-18 21:15:43 -0400

    Add files via upload

---
+++ meu_app.py
@@ -0,0 +1,13 @@
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from TESTE;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NOME}") ## has a :{row.pet}:")
